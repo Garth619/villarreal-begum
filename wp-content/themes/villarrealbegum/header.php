fa7a6591fcda1a3e868e30e60d48fb1c5f53b507
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-15 11:24:47 -0800

    push

--- wp-content/themes/villarrealbegum/header.php
+++ wp-content/themes/villarrealbegum/header.php
@@ -27,18 +27,20 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-<!--
+
 <link rel="stylesheet" href="https://use.typekit.net/hhs0ycw.css">
 <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,800&display=swap" rel="stylesheet">
--->
+
 
 <style>
 
 <?php the_field( 'review_css','option'); ?>
 
+/*
  @import url("https://use.typekit.net/hhs0ycw.css");
  
  @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,800&display=swap');
+*/
 
 </style>
 
@@ -52,10 +54,27 @@
 
 <body <?php body_class(); ?>>
 	
-	
-	<span class="">Garrett</span>
-	
 	<header>
+		
+		<div class="header_inner">
+			
+			<div class="header_left">
+				
+				<a class="logo" href="<?php bloginfo('url');?>">
+					
+					<span class="logo_title">Villarreal <span>&</span> Begum</span>
+					
+				</a><!-- logo -->
+				
+			</div><!-- header_left -->
+			
+			<div class="header_right">
+				
+				
+				
+			</div><!-- header_right -->
+			
+		</div><!-- header_inner -->
 		
 		<?php // wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'main_menu' ) ); ?>
 		
