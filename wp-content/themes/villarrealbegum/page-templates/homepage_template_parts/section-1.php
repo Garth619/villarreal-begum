010ec5415acbbba594b9c8d494d23b1d078ea7e7
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-16 11:31:24 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-1.php
@@ -1,7 +1,40 @@
 <section id="section_one">
 	
+	<div class="sec_one_content">
 		
-
+		<div class="sec_one_left">
+			
+			<span class="sec_one_subheader">texas personal injury lawyers</span><!-- sec_one_subheader -->
+			
+			<span class="sec_one_largeheader"><span>if we don’t win,</span> we don’t get paid.</span><!-- sec_one_largeheader -->
+			
+			<a class="consult_button" href="#consultation">Free Consultation</a><!-- consult_button -->
+			
+		</div><!-- sec_one_left -->
+		
+		<div class="sec_one_right">
+			
+			<div class="atts_wrapper">
+				
+				
+				
+			</div><!-- atts_wrapper -->
+			
+		</div><!--  sec_one_right -->
+		
+	</div><!-- sec_one_content -->
+	
+	<div class="parallax">
+			
+		<img id="para_one" src="" alt="Parralax Img One"/>
+		
+		<img id="para_two" src="" alt="Parralax Img One"/>
+				
+		<img id="para_three" src="" alt="Parralax Img One"/>
+				
+	</div><!-- parallax -->
+	
+	<img class="hero_img" src="<?php bloginfo('template_directory');?>/images/header_bg_desk.jpg"/>
 	
 </section><!-- section_one -->
 
---
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-2.php
@@ -0,0 +1,5 @@
+<section id="section_two">
+	
+	
+	
+</section><!-- section_two -->
