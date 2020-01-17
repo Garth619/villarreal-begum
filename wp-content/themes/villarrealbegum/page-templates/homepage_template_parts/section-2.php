5512382726ab0c4aa02be015a3235aefd163e8e5
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-17 15:05:58 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-2.php
@@ -6,7 +6,7 @@
 			
 			<div class="sec_two_slider">
 			
-				<div class="sec_two_slide">
+				<div class="sec_two_slide one">
 					
 					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
 					
@@ -20,43 +20,43 @@
 					
 				</div><!-- sec_two_slide -->
 				
-				<div class="sec_two_slide">
+				<div class="sec_two_slide two">
 					
-					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
+					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_2.svg"); ?>
 					
 					<div class="sec_two_slide_inner">
 						
-						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
+						<span class="sec_two_slide_title">Results</span><!-- sec_two_slide_title -->
 						
-						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
+						<span class="sec_two_description">We have more than $500 million in verdicts and settlements. Our firm is small enough to care but large enough to invest in our cases.</span><!-- sec_two_description -->
 						
 					</div><!-- sec_two_slide_inner -->
 					
 				</div><!-- sec_two_slide -->
 				
-				<div class="sec_two_slide">
+				<div class="sec_two_slide three">
 					
-					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
+					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_3.svg"); ?>
 					
 					<div class="sec_two_slide_inner">
 						
-						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
+						<span class="sec_two_slide_title">Tough</span><!-- sec_two_slide_title -->
 						
-						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
+						<span class="sec_two_description">We dedicate our time and effort towards helping people in San Antonio, Texas find justice for personal negligence encountered while living their normal, everyday lives.</span><!-- sec_two_description -->
 						
 					</div><!-- sec_two_slide_inner -->
 					
 				</div><!-- sec_two_slide -->
 				
-				<div class="sec_two_slide">
+				<div class="sec_two_slide four">
 					
-					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
+					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_4.svg"); ?>
 					
 					<div class="sec_two_slide_inner">
 						
-						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
+						<span class="sec_two_slide_title">Aggressive</span><!-- sec_two_slide_title -->
 						
-						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
+						<span class="sec_two_description">The Texas Law Guns understand the injustice of this fight, so we step in to give you a fair shot at obtaining justice and compensation.</span><!-- sec_two_description -->
 						
 					</div><!-- sec_two_slide_inner -->
 					
