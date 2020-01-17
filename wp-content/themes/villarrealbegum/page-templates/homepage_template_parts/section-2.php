bce0998db77ca17112a5d2d5ca819dfc3211731d
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-17 14:23:20 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-2.php
@@ -1,5 +1,71 @@
 <section id="section_two">
 	
-	
+	<div class="sec_two_inner">
+		
+		<div class="sec_two_slider_wrapper">
+			
+			<div class="sec_two_slider">
+			
+				<div class="sec_two_slide">
+					
+					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
+					
+					<div class="sec_two_slide_inner">
+						
+						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
+						
+						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
+						
+					</div><!-- sec_two_slide_inner -->
+					
+				</div><!-- sec_two_slide -->
+				
+				<div class="sec_two_slide">
+					
+					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
+					
+					<div class="sec_two_slide_inner">
+						
+						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
+						
+						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
+						
+					</div><!-- sec_two_slide_inner -->
+					
+				</div><!-- sec_two_slide -->
+				
+				<div class="sec_two_slide">
+					
+					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
+					
+					<div class="sec_two_slide_inner">
+						
+						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
+						
+						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
+						
+					</div><!-- sec_two_slide_inner -->
+					
+				</div><!-- sec_two_slide -->
+				
+				<div class="sec_two_slide">
+					
+					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
+					
+					<div class="sec_two_slide_inner">
+						
+						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
+						
+						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
+						
+					</div><!-- sec_two_slide_inner -->
+					
+				</div><!-- sec_two_slide -->
+			
+			</div><!-- sec_two_slider -->
+			
+		</div><!-- sec_two_slider_wrapper -->
+		
+	</div><!-- sec_two_inner -->
 	
 </section><!-- section_two -->
