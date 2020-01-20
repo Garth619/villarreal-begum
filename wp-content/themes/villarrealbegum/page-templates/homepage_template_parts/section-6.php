9bfb26bc32e6285578638171f35b25773b0ba578
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-20 11:56:31 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-6.php
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-6.php
@@ -10,7 +10,29 @@
 			
 		</div><!-- sec_six_left -->
 		
-		<div class="sec_six_right"></div><!-- sec_six_right -->
+		<div class="sec_six_right">
+			
+			<div class="sec_six_slider">
+				
+				<div class="sec_six_slide">
+					
+					<div class="star_box">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/star-white.svg"/>
+						
+					</div><!-- star_box -->
+					
+					<span class="sec_six_slide_title">I had a very bad accident.</span><!-- sec_six_slide_title -->
+					
+					<div class="black_bar"></div><!-- black_bar -->
+					
+					<span class="sec_six_description">This firm helped me get back on my feet, took care of my doctors appointments and paid me out a deserved settlement.</span><!-- sec_six_description -->
+					
+				</div><!-- sec_six_slide -->
+				
+			</div><!-- sec_six_slider -->
+			
+		</div><!-- sec_six_right -->
 		
 	</div><!-- sec_six_inner -->
 	
