3b896cde75b6474e395538890f585d55d2b9fb6f
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-19 19:16:00 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/template-home.php
+++ wp-content/themes/villarrealbegum/page-templates/template-home.php
@@ -9,6 +9,7 @@
 	get_template_part( 'page-templates/homepage_template_parts/section', '2' );
 	get_template_part( 'page-templates/homepage_template_parts/section', '3' );
 	get_template_part( 'page-templates/homepage_template_parts/section', '4' );
+	get_template_part( 'page-templates/homepage_template_parts/section', '5' );
 	
 	
 	get_footer(); ?>
