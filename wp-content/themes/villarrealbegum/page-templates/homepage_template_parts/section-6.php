76627ec1b26efd2952d7e823a8498ba4b2c58379
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-20 11:42:24 -0800

    push

---
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-6.php
@@ -0,0 +1,21 @@
+<section id="section_six">
+	
+	<div class="sec_six_inner">
+		
+		<div class="sec_six_left">
+			
+			<span class="sec_six_title">What Our Clients Have to Say</span><!-- sec_six_title -->
+			
+			<a class="reviews_button desktop" href="">View All Testimonials</a><!-- reviews_button -->
+			
+		</div><!-- sec_six_left -->
+		
+		<div class="sec_six_right"></div><!-- sec_six_right -->
+		
+	</div><!-- sec_six_inner -->
+	
+	<div class="sec_six_bottom"></div><!-- sec_six_bottom -->
+	
+	<img class="sec_six_bg" src="<?php bloginfo('template_directory');?>/images/test_bg_desk.jpg"/>
+	
+</section><!-- section_six -->
