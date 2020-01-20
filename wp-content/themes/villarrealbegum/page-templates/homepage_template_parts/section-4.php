29b82bf29a7cde3ec76ac257dd1279778e63021c
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-19 17:30:04 -0800

    push

---
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-4.php
@@ -0,0 +1,13 @@
+<section id="section_four">
+	
+	<div class="sec_four_inner">
+		
+		<span class="sec_four_subheader">Villarreal & Begum</span><!-- sec_four_subheader -->
+		
+		<span class="sec_four_largehader">we believe in treating all of our clients like family</span><!-- sec_four_largehader -->
+		
+		<a class="meet_button" href="">Meet the Attorneys</a>
+		
+	</div><!-- sec_four_inner -->
+	
+</section><!-- section_four -->
--- wp-content/themes/villarrealbegum/page-templates/template-home.php
+++ wp-content/themes/villarrealbegum/page-templates/template-home.php
@@ -8,6 +8,7 @@
 	get_template_part( 'page-templates/homepage_template_parts/section', '1' );
 	get_template_part( 'page-templates/homepage_template_parts/section', '2' );
 	get_template_part( 'page-templates/homepage_template_parts/section', '3' );
+	get_template_part( 'page-templates/homepage_template_parts/section', '4' );
 	
 	
 	get_footer(); ?>
