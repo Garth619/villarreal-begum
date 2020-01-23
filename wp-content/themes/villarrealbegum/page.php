561c1caf6e0f2795abb78f37a57cb3f61eca0ff3
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-22 18:04:46 -0800

    push

--- wp-content/themes/villarrealbegum/page.php
+++ wp-content/themes/villarrealbegum/page.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
+<div id="internal_main">
+	
+	<div class="internal_banner">
+		
+		<img class="internal_hero" src="<?php bloginfo('template_directory');?>/images/header_bg_desk.jpg"/>
+		
+	</div><!-- internal_banner -->
+	
+</div><!-- internal_main -->
+
 
 		
 	<?php get_template_part( 'loop', 'page' ); ?>
