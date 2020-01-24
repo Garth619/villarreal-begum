70567ccf6e58a0bae42918d4cb7470054231fcc6
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-24 13:59:31 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/template-padirectory.php
+++ wp-content/themes/villarrealbegum/page-templates/template-padirectory.php
@@ -5,8 +5,18 @@
 get_header(); ?>
 
 
-		
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+<div id="internal_main">
 
+<div class="single_col_container">
+	
+		<div class="pa_directory_wrapper">
+			
+			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+			
+		</div><!-- pa_directory_wrapper -->
+		
+</div><!-- single_col_container -->
+	
+</div><!-- internal_main -->
 
 <?php get_footer(); ?>
