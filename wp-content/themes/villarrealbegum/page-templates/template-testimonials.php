3813b39be8df5a45c2b30d1cf7542e3c4aa2b3ee
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-24 15:38:21 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/template-testimonials.php
+++ wp-content/themes/villarrealbegum/page-templates/template-testimonials.php
@@ -5,6 +5,108 @@
 get_header(); ?>
 
 
+<div id="internal_main">
 
+<div class="single_col_container">
+	
+		<h1 class="deluxe_reg_header"><?php the_title();?></h1><!-- deluxe_reg_header -->
+	
+		<div class="testi_wrapper">
+			
+			<div class="single_review">
+				
+				<div class="review_star_wrapper">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					
+				</div><!-- review_star_wrapper -->
+				
+				<span class="review_title">Customer service at this law firm is excellent.</span><!-- review_title -->
+				
+				<div class="review_content">
+					
+					<p>I would definitely refer this law firm to any one of my family members and friends.  Marie Rodriguez answered all my questions with no hesitation, she didn’t take days to call me back and she was friendly, proffesional and courteous. She made me feel like family and that she cared. This firm is a five star.</p>
+					
+					<div class="review_meta">
+						
+						<span class="review_name">Claudia Chave</span>
+						
+						<span class="review_client">Client</span>
+						
+					</div><!-- review_meta -->
+					
+				</div><!-- review_content -->
+				
+			</div><!-- single_review -->
+			
+			<div class="single_review">
+				
+				<div class="review_star_wrapper">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					
+				</div><!-- review_star_wrapper -->
+				
+				<span class="review_title">Customer service at this law firm is excellent.</span><!-- review_title -->
+				
+				<div class="review_content">
+					
+					<p>I would definitely refer this law firm to any one of my family members and friends.  Marie Rodriguez answered all my questions with no hesitation, she didn’t take days to call me back and she was friendly, proffesional and courteous. She made me feel like family and that she cared. This firm is a five star.</p>
+										
+					<div class="review_meta">
+						
+						<span class="review_name">Claudia Chave</span>
+						
+						<span class="review_client">Client</span>
+						
+					</div><!-- review_meta -->
+					
+				</div><!-- review_content -->
+				
+			</div><!-- single_review -->
+			
+			<div class="single_review">
+				
+				<div class="review_star_wrapper">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					<img src="<?php bloginfo('template_directory');?>/images/star.svg"/>
+					
+				</div><!-- review_star_wrapper -->
+				
+				<span class="review_title">Customer service at this law firm is excellent.</span><!-- review_title -->
+				
+				<div class="review_content">
+					
+					<p>I would definitely refer this law firm to any one of my family members and friends.  Marie Rodriguez answered all my questions with no hesitation, she didn’t take days to call me back and she was friendly, proffesional and courteous. She made me feel like family and that she cared. This firm is a five star.</p>
+					
+					<div class="review_meta">
+						
+						<span class="review_name">Claudia Chave</span>
+						
+						<span class="review_client">Client</span>
+						
+					</div><!-- review_meta -->
+					
+				</div><!-- review_content -->
+				
+			</div><!-- single_review -->
+			
+		</div><!-- testi_wrapper -->
+		
+</div><!-- single_col_container -->
+	
+</div><!-- internal_main -->
 
 <?php get_footer(); ?>
