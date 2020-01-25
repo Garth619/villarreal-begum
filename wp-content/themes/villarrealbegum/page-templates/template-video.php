27ef7f3840209434fe12abd1955307438889c7d9
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-24 17:39:02 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/template-video.php
+++ wp-content/themes/villarrealbegum/page-templates/template-video.php
@@ -4,6 +4,50 @@
 
 get_header(); ?>
 
+<div id="internal_main">
 
+<div class="single_col_container">
+	
+		<h1 class="deluxe_reg_header"><?php the_title();?></h1><!-- deluxe_reg_header -->
+	
+		<div class="video_center_wrapper">
+			
+			<span class="video_cat_title"><span>English Testimonials</span></span><!-- video_cat_title -->
+			
+			<div class="video_wrapper">
+				
+				<div class="single_video">
+				
+					<div class="play_wrapper">
+						
+						<div class="play_button"></div><!-- play_button -->
+						
+					</div><!-- play_wrapper -->
+				
+				</div><!-- single_video -->
+				
+				<div class="single_video">
+				
+				</div><!-- single_video -->
+				
+				<div class="single_video">
+				
+				</div><!-- single_video -->
+				
+				<div class="single_video">
+				
+				</div><!-- single_video -->
+				
+				<div class="single_video">
+				
+				</div><!-- single_video -->
+				
+			</div><!-- video_wrapper -->
+			
+		</div><!-- video_center_wrapper -->
+		
+</div><!-- single_col_container -->
+	
+</div><!-- internal_main -->
 
 <?php get_footer(); ?>
