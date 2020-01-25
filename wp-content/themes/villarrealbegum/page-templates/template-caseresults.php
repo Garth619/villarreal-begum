a85c5ae8b291e6387eb9e93adf6c17906a789ab9
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-24 19:37:28 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/template-caseresults.php
+++ wp-content/themes/villarrealbegum/page-templates/template-caseresults.php
@@ -5,7 +5,79 @@
 get_header(); ?>
 
 
-						
+<div id="internal_main">
+
+<div class="single_col_container">
+	
+		<h1 class="deluxe_line_header"><span><?php the_title();?></span></h1><!-- deluxe_reg_header -->
+	
+		<div class="case_results_wrapper">
+			
+			<div class="single_results">
+				
+				<img class="trophie" src="<?php bloginfo('template_directory');?>/images/results_icon.svg"/>
+				
+				<span class="amount">$3.5m</span><!-- amount -->
+				
+				<span class="type">Product Defect Claim</span><!-- type -->
+				
+			</div><!-- single_results -->
+			
+			<div class="single_results">
+				
+				<img class="trophie" src="<?php bloginfo('template_directory');?>/images/results_icon.svg"/>
+				
+				<span class="amount">$3.5m</span><!-- amount -->
+				
+				<span class="type">Product Defect Claim</span><!-- type -->
+				
+			</div><!-- single_results -->
+			
+			<div class="single_results">
+				
+				<img class="trophie" src="<?php bloginfo('template_directory');?>/images/results_icon.svg"/>
+				
+				<span class="amount">$3.5m</span><!-- amount -->
+				
+				<span class="type">Product Defect Claim</span><!-- type -->
+				
+			</div><!-- single_results -->
+			
+			<div class="single_results">
+				
+				<img class="trophie" src="<?php bloginfo('template_directory');?>/images/results_icon.svg"/>
+				
+				<span class="amount">$3.5m</span><!-- amount -->
+				
+				<span class="type">Product Defect Claim</span><!-- type -->
+				
+			</div><!-- single_results -->
+			
+			<div class="single_results">
+				
+				<img class="trophie" src="<?php bloginfo('template_directory');?>/images/results_icon.svg"/>
+				
+				<span class="amount">$3.5m</span><!-- amount -->
+				
+				<span class="type">Product Defect Claim</span><!-- type -->
+				
+			</div><!-- single_results -->
+			
+			<div class="single_results">
+				
+				<img class="trophie" src="<?php bloginfo('template_directory');?>/images/results_icon.svg"/>
+				
+				<span class="amount">$3.5m</span><!-- amount -->
+				
+				<span class="type">Product Defect Claim</span><!-- type -->
+				
+			</div><!-- single_results -->
+			
+		</div><!-- case_results_wrapper -->
+		
+</div><!-- single_col_container -->
+	
+</div><!-- internal_main -->	
 			  
 
 
--- wp-content/themes/villarrealbegum/page-templates/template-video.php
+++ wp-content/themes/villarrealbegum/page-templates/template-video.php
@@ -12,42 +12,46 @@ get_header(); ?>
 	
 		<div class="video_center_wrapper">
 			
-			<span class="video_cat_title"><span>English Testimonials</span></span><!-- video_cat_title -->
-			
-			<div class="video_wrapper">
+			<?php if(get_field('video_category')): ?>
+			 
+				<?php while(has_sub_field('video_category')): ?>
+			 
+					<span class="video_cat_title"><span><?php the_sub_field( 'video_category_title' ); ?></span></span><!-- video_cat_title -->
+					
+					<?php if(get_sub_field('video')): ?>
+					
+						<div class="video_wrapper">
+					 
+						<?php while(has_sub_field('video')): ?>
+					 
+							<div class="single_video">
+					
+								<div class="mywistia wistia_embed wistia_async_<?php the_sub_field( 'wistia_id' ); ?> popover=true popoverContent=thumbnail"></div><!-- mywistia -->
 				
-				<div class="single_video">
-				
-					<div class="play_wrapper">
+									<div class="play_wrapper">
 						
-						<div class="play_button"></div><!-- play_button -->
+										<div class="play_button"></div><!-- play_button -->
 						
-					</div><!-- play_wrapper -->
+									</div><!-- play_wrapper -->
 				
-				</div><!-- single_video -->
-				
-				<div class="single_video">
-				
-				</div><!-- single_video -->
-				
-				<div class="single_video">
-				
-				</div><!-- single_video -->
-				
-				<div class="single_video">
-				
-				</div><!-- single_video -->
-				
-				<div class="single_video">
-				
-				</div><!-- single_video -->
-				
-			</div><!-- video_wrapper -->
+							</div><!-- single_video -->
+					    
+						<?php endwhile; ?>
+						
+						</div><!-- video_wrapper -->
+					 
+					<?php endif; ?>
+			    
+				<?php endwhile; ?>
+			 
+			<?php endif; ?>
 			
 		</div><!-- video_center_wrapper -->
 		
 </div><!-- single_col_container -->
 	
 </div><!-- internal_main -->
+
+<script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>
 
 <?php get_footer(); ?>
