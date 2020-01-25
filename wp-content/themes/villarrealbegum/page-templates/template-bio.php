0af2a7b32b50b38abfff5ac346389dcdd27ff1c5
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-25 14:50:16 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/template-bio.php
+++ wp-content/themes/villarrealbegum/page-templates/template-bio.php
@@ -5,8 +5,57 @@
 get_header(); ?>
 
 
+<div id="internal_main">
 
-	<?php get_template_part( 'loop', 'page' ); ?>
+<div class="att_container">
+	
+	<div class="att_bio_wrapper">
+		
+		<div class="att_bio_left">
+			
+			<h1 class="att_bio_header"><span><?php the_title();?></span></h1><!-- att_bio_header -->
+			
+			<span class="att_bio_position">Attorney</span><!-- att_bio_position -->
+			
+			<div class="att_bio_intro">
+				
+				<p>Alex has tried over 50 trials to verdict and tried or settled over one hundred million dollars in cases. He was named a top ten personal injury lawyers under 40 in the nation by the National Academy of Personal Injury Attorneys in both 2014 and 2015. In 2013, 2014 and 2015 he was named one of the best personal injury lawyers in a poll voted on only by fellow attorneys and published in SA Scene Magazine as one of the “best San Antonio personal injury lawyers“.</p>
+				
+			</div><!-- att_bio_intro -->
+			
+			<div class="att_bio_content content">
+				
+				<p>Alexander Begum is a founding shareholder of Villareal & Begum. Born and raised in Texas, Alex continues to practice law in the state where he grew up, with offices in San Antonio, Laredo, and McAllen. He earned his bachelor’s degree in Marketing and Finance from Trinity University in San Antonio and went on to study finance and legal writing at Harvard University before returning to Texas to attend Texas Tech University. While there, he earned a Masters of Business Administration with a concentration in Finance and a Doctorate of Jurisprudence.</p>
+
+				<p>He later attended the Trial Lawyers College, an intense program that is open only to those lawyers who are dedicated to representing individuals, particularly the poor, injured, and otherwise needy. This program is not open to people who represent insurance companies, governments or large business. For Alex, who believes in the jury system and the importance of being not only a skilled attorney but caring for his clients on a human level, this program was too great an opportunity to miss. After being only one of 50 lawyers accepted into the program, he attended the nationally renowned Trial Lawyers College.</p>
+
+				<p>Alex is passionate about helping others, which is why he has pursued a career in the law. He continues to find new ways to be of service, including regularly offering pro bono legal service to those in need. His willingness to serve others without concern for himself saved an entire Texas community faced with the potential loss of their homes. Outside of the legal sphere, Alex also serves as a board member for several charitable organizations.</p>
+				
+			</div><!-- att_bio_content -->
+			
+		</div><!-- att_bio_left -->
+		
+		<div class="att_bio_right">
+			
+			<div class="att_image">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/attorney_bio_img.jpg"/>
+				
+				<div>
+					
+					<img class="att_badge" src="<?php bloginfo('template_directory');?>/images/header_law_guns.png"/>
+				
+				</div>
+				
+			</div><!-- att_image -->
+			
+		</div><!-- att_bio_right -->
+		
+	</div><!-- att_bio_wrapper -->
+		
+</div><!-- att_container -->
+	
+</div><!-- internal_main -->
+
 					 
-					 	
 <?php get_footer(); ?>
