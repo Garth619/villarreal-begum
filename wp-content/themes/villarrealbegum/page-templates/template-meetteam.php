2046211411e863ff5a21a84d0c69884a4e8a9e0c
Garrett Cullen <Garth619@users.noreply.github.com>
2020-01-25 19:52:19 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/template-meetteam.php
+++ wp-content/themes/villarrealbegum/page-templates/template-meetteam.php
@@ -4,7 +4,119 @@
 
 get_header(); ?>
 
+<div id="internal_main">
 
+<div class="single_col_container">
+	
+		<h1 class="deluxe_reg_header"><?php the_title();?></h1><!-- deluxe_reg_header -->
+	
+		<div class="meet_the_team_wrapper">
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="att_img_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney_bio_img.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+						
+					</div><!-- att_img_wrapper -->
+					
+					<span class="single_att_title"><span>Alex Begum</span></span><!-- single_att_title -->
+					
+					<span class="single_position">Attorney</span><!-- single_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="att_img_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney_bio_img.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+						
+					</div><!-- att_img_wrapper -->
+					
+					<span class="single_att_title"><span>Alex Begum</span></span><!-- single_att_title -->
+					
+					<span class="single_position">Attorney</span><!-- single_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="att_img_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney_bio_img.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+						
+					</div><!-- att_img_wrapper -->
+					
+					<span class="single_att_title"><span>Alex Begum</span></span><!-- single_att_title -->
+					
+					<span class="single_position">Attorney</span><!-- single_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="att_img_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney_bio_img.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+						
+					</div><!-- att_img_wrapper -->
+					
+					<span class="single_att_title"><span>Alex Begum</span></span><!-- single_att_title -->
+					
+					<span class="single_position">Attorney</span><!-- single_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+					
+					<div class="att_img_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/attorney_bio_img.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+						
+					</div><!-- att_img_wrapper -->
+					
+					<span class="single_att_title"><span>Alex Begum</span></span><!-- single_att_title -->
+					
+					<span class="single_position">Attorney</span><!-- single_position -->
+					
+				</a>
+				
+			</div><!-- single_att -->
+			
+		</div><!-- meet_the_team_wrapper -->
+		
+</div><!-- single_col_container -->
+	
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
