45427538caec6ce80dbe875ec140e94defeb35c3
Garrett Cullen <Garth619@users.noreply.github.com>
2020-02-03 18:09:19 -0800

    push

--- wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-2.php
@@ -5,62 +5,30 @@
 		<div class="sec_two_slider_wrapper">
 			
 			<div class="sec_two_slider">
-			
-				<div class="sec_two_slide one">
-					
-					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_1.svg"); ?>
-					
-					<div class="sec_two_slide_inner">
-						
-						<span class="sec_two_slide_title">Dedicated</span><!-- sec_two_slide_title -->
-						
-						<span class="sec_two_description">Standing up for those injured by the negligence or carelessness of others is our passion, and with our extensive legal experience and dedicated team we fight for you!</span><!-- sec_two_description -->
-						
-					</div><!-- sec_two_slide_inner -->
-					
-				</div><!-- sec_two_slide -->
 				
-				<div class="sec_two_slide two">
+				<?php if(get_field('section_two_selling_points_villarreal')): ?>
+				 
+					<?php while(has_sub_field('section_two_selling_points_villarreal')): ?>
+				 
+						<div class="sec_two_slide <?php the_sub_field( 'class_name' ); ?>">
+							
+							<?php $svg = get_sub_field( 'svg' );?>
 					
-					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_2.svg"); ?>
+							<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/".$svg.""); ?>
 					
-					<div class="sec_two_slide_inner">
+							<div class="sec_two_slide_inner">
 						
-						<span class="sec_two_slide_title">Results</span><!-- sec_two_slide_title -->
+								<span class="sec_two_slide_title"><?php the_sub_field( 'title' ); ?></span><!-- sec_two_slide_title -->
 						
-						<span class="sec_two_description">We have more than $500 million in verdicts and settlements. Our firm is small enough to care but large enough to invest in our cases.</span><!-- sec_two_description -->
+								<span class="sec_two_description"><?php the_sub_field( 'description' ); ?></span><!-- sec_two_description -->
 						
-					</div><!-- sec_two_slide_inner -->
+							</div><!-- sec_two_slide_inner -->
 					
-				</div><!-- sec_two_slide -->
-				
-				<div class="sec_two_slide three">
-					
-					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_3.svg"); ?>
-					
-					<div class="sec_two_slide_inner">
-						
-						<span class="sec_two_slide_title">Tough</span><!-- sec_two_slide_title -->
-						
-						<span class="sec_two_description">We dedicate our time and effort towards helping people in San Antonio, Texas find justice for personal negligence encountered while living their normal, everyday lives.</span><!-- sec_two_description -->
-						
-					</div><!-- sec_two_slide_inner -->
-					
-				</div><!-- sec_two_slide -->
-				
-				<div class="sec_two_slide four">
-					
-					<?php echo file_get_contents("wp-content/themes/villarrealbegum/images/sp_icon_4.svg"); ?>
-					
-					<div class="sec_two_slide_inner">
-						
-						<span class="sec_two_slide_title">Aggressive</span><!-- sec_two_slide_title -->
-						
-						<span class="sec_two_description">The Texas Law Guns understand the injustice of this fight, so we step in to give you a fair shot at obtaining justice and compensation.</span><!-- sec_two_description -->
-						
-					</div><!-- sec_two_slide_inner -->
-					
-				</div><!-- sec_two_slide -->
+						</div><!-- sec_two_slide -->
+				    
+					<?php endwhile; ?>
+				 
+				<?php endif; ?>
 			
 			</div><!-- sec_two_slider -->
 			
--- wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-3.php
+++ wp-content/themes/villarrealbegum/page-templates/homepage_template_parts/section-3.php
@@ -2,11 +2,11 @@
 	
 	<div class="sec_three_inner">
 		
-		<h1 class="sec_three_title">Texas Personal Injury Lawyers</h1>
+		<h1 class="sec_three_title"><?php the_field( 'section_three_title' ); ?></h1>
 		
 		<div class="sec_three_intro_wrapper">
 			
-			<p>The Villarreal & Begum Law Firm dedicates their time and effort towards helping people in San Antonio, Texas find justice for personal negligence encountered while living their normal, everyday lives.</p>
+			<?php the_field( 'section_three_intro' ); ?>
 			
 		</div><!-- sec_three_intro_wrapper -->
 		
@@ -14,34 +14,34 @@
 			
 			<div class="sec_three_left">
 				
-				<span class="sec_three_left_title">Practice Areas</span><!-- sec_three_left_title -->
+				<span class="sec_three_left_title"><?php the_field( 'section_three_sidebar_title' ); ?></span><!-- sec_three_left_title -->
 				
-				<ul>
-					<li><a href="">auto accident</a></li>
-					<li><a href="">drunk driving accident</a></li>
-					<li><a href="">18 wheeler accident</a></li>
-					<li><a href="">motorcycle accident</a></li>
-					<li><a href="">slip and fall accident</a></li>
-					<li><a href="">wrongful death</a></li>
-					<li><a href="">nursing home abuse</a></li>
-					<li><a href="<?php the_permalink(103);?>">view all</a></li>
-				</ul>
+				<?php if(get_field('section_three_sidebar_items')): ?>
+				
+					<ul>
+					
+					<?php while(has_sub_field('section_three_sidebar_items')): ?>
+					 
+						<li><a href="<?php the_sub_field( 'pa_link' ); ?>"><?php the_sub_field( 'title' ); ?></a></li>
+					
+						<?php endwhile; ?>
+						
+							<?php if(get_field('section_three_sidebar_view_all')) { ?>
+					 
+								<li class="view_all_pa"><a href="<?php the_field( 'section_three_sidebar_view_all' ); ?>">view all</a></li>
+							
+							<?php } ?>
+					
+					</ul>
+				
+				<?php endif; ?>
 				
 			</div><!-- sec_three_left -->
 			
 			<div class="sec_three_right content">
 				
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vehicula ornare lacus vel tincidunt. Aenean consectetur tellus sit amet tempus egestas. Nam varius dignissim lobortis. Mauris imperdiet ipsum sed rhoncus dapibus.</p>
-
-				<p>Aenean consectetur tellus sit amet tempus egestas. Nam varius dignissim lobortis. Mauris imperdiet ipsum sed rhoncus dapibus. Mauris ultricies lectus nec tellus aliquet, quis maximus dolor euismod. Duis tristique orci vel auctor dapibus. Pellentesque vitae quam sed lorem blandit interdum a vel nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-
-
-				<p><strong>Cras egestas viverra porta. Praesent vel odio ultricies, mollis nisl in, congue ante. Nullam neque mi, condimentum viverra nunc eget, aliquet fermentum risus. Nam nunc mauris</strong></p>
 				
-				
-				
-				
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vehicula ornare lacus vel tincidunt. Aenean consectetur tellus sit amet tempus egestas. Nam varius dignissim lobortis. Mauris imperdiet ipsum sed rhoncus dapibus. Mauris ultricies lectus nec tellus aliquet, quis maximus dolor euismod. Duis tristique orci vel auctor dapibus. Pellentesque vitae quam sed lorem blandit interdum a vel nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum faucibus mi sed efficitur elementum. In imperdiet leo ut quam pretium, non euismod mauris suscipit. Sed nibh dui, gravida quis massa vel, maximus accumsan mi. In nec maximus augue.</p>
+				<?php the_field( 'section_three_content' ); ?>
 	
 				
 			</div><!-- sec_three_right -->
