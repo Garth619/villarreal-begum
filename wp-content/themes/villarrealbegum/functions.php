e5d5ec788c616ec4d06ff61f44dd06a48b87912f
Garrett Cullen <Garth619@users.noreply.github.com>
2020-03-10 16:09:16 -0700

    push

--- wp-content/themes/villarrealbegum/functions.php
+++ wp-content/themes/villarrealbegum/functions.php
@@ -11,7 +11,7 @@
 function load_my_styles_scripts() {
   
     
-    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', '', 5, 'all' ); 
+    //wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', '', 5, 'all' ); 
     
 
     // disables jquery then registers it again to go into footer
@@ -136,7 +136,7 @@ add_action("gform_enqueue_scripts", "deregister_scripts");
  
 
 
-/*
+
 function internal_css_print() {
    echo '<style>';
    
@@ -147,7 +147,7 @@ function internal_css_print() {
 
 
 add_action( 'wp_head', 'internal_css_print' );
-*/
+
 
 
 
